211f8cbd9a9195055312f2d88ea817b2463abcfa
tjxj <1030999281@qq.com>
2023-12-22 10:05:59 +0800

    1.0

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# 潮流周刊
+# 机器学习周刊
 
+> 关注Python、机器学习、深度学习、大模型，欢迎订阅
 
-Fork from 工程师 Tw93 的潮流周刊：https://github.com/tw93/weekly
+* [第 03 期 - 如何构建高效的RAG系统](https://weekly.zhanglearning.com/posts/03-如何构建高效的RAG系统)
+* [第 02 期 - 300个机器学习应用案例](https://weekly.zhanglearning.com/posts/02-300个机器学习应用案例)
+* [第 01 期 - 超完整数据科学资料合集](https://weekly.zhanglearning.com/posts/01-超完整数据科学资料合集)
--- build.py
+++ build.py
@@ -2,40 +2,27 @@ import os
 import httpx
 import re
 import urllib.parse
-import datetime
 
 def fetch_ci_time(filePath):
     entries = httpx.get("https://api.github.com/repos/tjxj/ml-weekly/commits?path=" + filePath + "&page=1&per_page=1")
-    ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
+    ciTime = entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
-    # return datetime.datetime.strptime(ciTime,"%Y-%m-%d")
 
 if __name__ == "__main__":
-  readmefile=open('README.md','w')
-  readmefile.write("# 机器学习周刊\n\n> 关注Python、机器学习、深度学习、大模型，欢迎订阅\n\n")
-  recentfile=open('RECENT.md','w')
+    readmefile = open('README.md', 'w')
+    readmefile.write("# 机器学习周刊\n\n> 关注Python、机器学习、深度学习、大模型，欢迎订阅\n\n")
 
-  for root, dirs, filenames in os.walk('./src/pages/posts'):
-    filenames = sorted(filenames, key=lambda x:float(re.findall("(\d+)",x)[0]), reverse=True)
+    for root, dirs, filenames in os.walk('./src/pages/posts'):
+        # Ensure that the file has a number to sort by, defaulting to 0 if not
+        filenames = sorted(filenames, key=lambda x: float(re.findall("(\d+)", x)[0]) if re.findall("(\d+)", x) else 0, reverse=True)
 
-  for index, name in enumerate(filenames):
-      if name.endswith('.md'):
-        filepath = urllib.parse.quote(name)
-        oldTitle = name.split('.md')[0]
-        url   = 'https://weekly.zhanglearning.com/posts/' + oldTitle
-        title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
-        readmeMd= '* [{}]({})\n'.format(title, url)
-        dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
-        num = int(oldTitle.split('-')[0])
-        if index < 5 :
-          if num < 100 :
-            modified = dateList[99-num]
-          else :
-            modified = fetch_ci_time('/src/pages/posts/' + filepath)
+    for name in filenames:
+        if name.endswith('.md'):
+            filepath = urllib.parse.quote(name)
+            oldTitle = name.split('.md')[0]
+            url = 'https://weekly.zhanglearning.com/posts/' + oldTitle
+            title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
+            readmeMd = '* [{}]({})\n'.format(title, url)
+            readmefile.write(readmeMd)
 
-          recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
-          recentfile.write(recentMd)
-        readmefile.write(readmeMd)
-
-  recentfile.close()
-  readmefile.close()
+    readmefile.close()
